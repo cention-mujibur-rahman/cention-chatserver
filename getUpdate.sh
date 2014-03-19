f81ba225070fcb965924b1937c0b91b79e8ce153
Adly Abdullah <adly_abdullah@cention.asia>
2014-03-20 06:55:59 +0800

    Modified getUpdate to use curl
    Chrome network inspector generates curl command.

--- getUpdate.sh
+++ getUpdate.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
+CMD=getUpdate
 HOST=localhost:9090
 ENVELOPE=0
+COOKIES=(
+	'cention-suiteWFUserID=2;'
+	'cention-suiteWFPassword=kW83un60RZvFwuehOluJiCws7VU9yOKcdTsSt060ZBI=;'
+	'webframework-session-id=webframework;'
+	'webframework-session-voucher=TLBHTFIQKEKFWGFP'
+)
 
 ARGV="$@"
 
@@ -16,19 +23,34 @@ while [ $# -gt 0 ];do
 		-envelope)
 			ENVELOPE=1
 			;;
+		-command)
+			CMD="$1"
+			shift
+			;;
 		*)	HOST=$op
 			;;
 	esac
 done
 
+chat_protocol () {
+	case "$1" in
+		getUpdate)
+			curl -s "http://${HOST}/Cention/chat/harmony/-/MCAM/GlobalChat.getUpdate" \
+				-H 'Origin: http://localhost' \
+				-H 'Accept-Encoding: gzip,deflate,sdch' \
+				-H 'Accept-Language: en-US,en;q=0.8,ms;q=0.6' \
+				-H 'Content-Type: application/x-www-form-urlencoded' \
+				-H 'Accept: */*' \
+				-H "Cookie: ${COOKIES[*]}" \
+				-H 'Connection: keep-alive' \
+				--data 'areas=1%2C3%2C2%2C4&users=2' \
+				--compressed
+			;;
+	esac
+}
+
 echo "[Response]========================================================"
-json=$(
-	wget "http://${HOST}/Cention/chat/harmony/-/MCAM/GlobalChat.getUpdate" \
-		--post-data='&areas=1%2C3%2C2%2C4&users=2' \
-		--no-cookie \
-		--header 'Cookie: cention-suiteWFUserID=2; cention-suiteWFPassword=kW83un60RZvFwuehOluJiCws7VU9yOKcdTsSt060ZBI=; webframework-session-id=webframework; webframework-session-voucher=TLBHTSTQKEMFGGFP' \
-		-O - 2> /dev/null
-)
+json=`chat_protocol "$CMD"`
 
 if [ $ENVELOPE -eq 0 ];then
 	echo $json | python -m json.tool |
