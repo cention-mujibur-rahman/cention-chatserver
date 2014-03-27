fb40c18a8031475f416396f1af315921bbb285e0
Adly Abdullah <adly_abdullah@cention.asia>
2014-03-27 15:48:21 +0800

    Improved agent_chat for general purpose queries

--- agent_chat.sh
+++ agent_chat.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-CMD=getUpdate
+CMD=''
+DATA=''
 HOST=localhost:9090
 ENVELOPE=0
 COOKIES=(
@@ -23,46 +24,90 @@ while [ $# -gt 0 ];do
 		-envelope)
 			ENVELOPE=1
 			;;
-		-command)
-			CMD="$1"
+		-host)
+			HOST="$1"
 			shift
 			;;
-		getUpdate)
-			CMD="$op"
+		-data)
+			DATA="$1"
+			shift
 			;;
-		*)	HOST="$op"
+		*)
+			CMD="$op"
 			;;
 	esac
 done
 
+
+mcam () {
+	method=$1
+	data=$2
+	curl -s "http://${HOST}/Cention/chat/harmony/-/MCAM/GlobalChat.${method}" \
+		-H 'Origin: http://localhost' \
+		-H 'Accept-Encoding: gzip,deflate,sdch' \
+		-H 'Accept-Language: en-US,en;q=0.8,ms;q=0.6' \
+		-H 'Content-Type: application/x-www-form-urlencoded' \
+		-H 'Accept: */*' \
+		-H "Cookie: ${COOKIES[*]}" \
+		-H 'Connection: keep-alive' \
+		--data "$data" \
+		--compressed
+}
+
+default () {
+	var="$1"
+	def="$2"
+	if [ "$var" == "" ];then
+		echo $2
+	else
+		echo $1
+	fi
+}
+
 chat_protocol () {
 	case "$1" in
 		getUpdate)
-			curl -s "http://${HOST}/Cention/chat/harmony/-/MCAM/GlobalChat.getUpdate" \
-				-H 'Origin: http://localhost' \
-				-H 'Accept-Encoding: gzip,deflate,sdch' \
-				-H 'Accept-Language: en-US,en;q=0.8,ms;q=0.6' \
-				-H 'Content-Type: application/x-www-form-urlencoded' \
-				-H 'Accept: */*' \
-				-H "Cookie: ${COOKIES[*]}" \
-				-H 'Connection: keep-alive' \
-				--data 'areas=1%2C3%2C2%2C4&users=2' \
-				--compressed
+			mcam getUpdate \
+				`default "$DATA" 'areas=1%2C3%2C2%2C4&users=2'`
+			;;
+		acquireSession)
+			mcam acquireSession \
+				`default "$DATA" 'session=20'`
+			;;
+		markAsSeen)
+			mcam markAsSeen \
+				`default "$DATA" 'sessions=20'`
+			;;
+		sendMessage)
+			mcam sendMessage \
+				`default "$DATA" 'session=20&text=hello'`
+			;;
+		finishChatSessionTag)
+			mcam finishChatSessionTag \
+				`default "$DATA" 'value=20&withTag=notag&tagid=-1&id=0'`
 			;;
 	esac
 }
 
-echo "[Response]========================================================"
+if [ "$CMD" == "" ];then
+	echo Agent chat client.
+	echo Available commands:
+	echo "  " getUpdate
+	echo "  " acquireSession -data session=NUMBER
+	echo "  " markAsSeen -data sessions=NUMBER
+	echo "  " sendMessage -data "'session=NUMBER&text=STRING'"
+	echo "  " finishChatSessionTag -data "'value=NUMBER&withTag=notag&tagid=-1&id=0'"
+	exit
+fi
+
 json=`chat_protocol "$CMD"`
 
 if [ $ENVELOPE -eq 0 ];then
-	echo $json | python -m json.tool |
-	awk '/"content"\s*:/ {gsub(/"content"\s*:/,"");gsub(/\\/,"");gsub(/"{/,"{");gsub(/",$/,"");print $0; exit}' |
-	python -m json.tool
+	echo $json | ./jsontool.fe -- -find mcam.channels[0].content -plain 2> /dev/null |
+	./jsontool.fe 2> /dev/null
 else
-	echo $json | python -m json.tool
+	echo $json | ./jsontool.fe 2> /dev/null
 fi
 
 echo
-echo "=================================================================="
 
